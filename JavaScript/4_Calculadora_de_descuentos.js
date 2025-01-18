31656351fe43f206dc45d06ab137b2179f53097e
ccarjim2909 <158286303+ccarjim2909@users.noreply.github.com>
2025-01-18 16:31:37 +0100

    Delete JavaScript directory

--- JavaScript/4_Calculadora_de_descuentos.js
+++
@@ -1,9 +0,0 @@
-const precio_original = parseFloat(prompt("Inserta el precio original del producto:"));
-
-const descuento = parseInt(prompt("Inserta el porcentaje de descuento a aplicar:"));
-
-const descuento_aplicado = (precio_original * descuento) / 100;
-
-const precio_final = precio_original - descuento_aplicado
-
-alert("El precio final del producto después de aplicar un descuento del " + descuento + "% es: "+ precio_final + " euros.");
